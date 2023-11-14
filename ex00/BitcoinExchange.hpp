f7b5400ba70f105b49b821b612b9502e84a274f1
hyemch <blossoming33@gmail.com>
2023-11-14 17:32:51 +0900

    [Feat] Makefile, ex00 main 작성

---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,19 @@
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <iostream>
+#include <map>
+
+class BitcoinExchange
+{
+public:
+	BitcoinExchange();
+	~BitcoinExchange();
+	BitcoinExchange(const BitcoinExchange& other);
+	BitcoinExchange& operator=(const BitcoinExchange& other);
+
+private:
+
+};
+
+#endif
