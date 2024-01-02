0d7d3f78df0d210459a0825598f6ff3b1f5c7fa2
hyemch <blossoming33@gmail.com>
2024-01-02 16:00:39 +0900

    [Feat] leak check 후 삭제

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,13 +1,7 @@
 #include "PmergeMe.hpp"
 
-void	leak(void)
-{
-	system("leaks PmergeMe");
-}
-
 int main(int argc, char* argv[])
 {
-	atexit(leak);
 	if (argc < 2)
 	{
 		std::cout << "Usage: ./PmergeMe [num1 num2 num3 ...]"<< std::endl;
